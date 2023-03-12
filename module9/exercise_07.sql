4c1e6f70a6b00f5ee69a13c901cfae4a96f4a2a6
Gabriel Van Dreel <gvandree@uncc.edu>
2023-03-12 14:59:00 -0400

    Completed seventh exercise

---
+++ module9/exercise_07.sql
@@ -0,0 +1,5 @@
+SELECT course.course_code, instructor.first_name, instructor.last_name, course.num_credits
+FROM course, instructor
+WHERE instructor.instructor_id = course.instructor_id AND course.course_id IN (
+    SELECT course_id FROM student_schedule WHERE student_id = 1
+);
