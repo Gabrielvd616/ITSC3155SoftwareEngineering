966e93ee84040a621a73b5128c4eb25b0cdb1c7a
Gabriel Van Dreel <gvandree@uncc.edu>
2023-03-12 14:54:27 -0400

    Completed fourth exercise

---
+++ module9/exercise_04.sql
@@ -0,0 +1,4 @@
+SELECT instructor.instructor_id, instructor.first_name, instructor.last_name
+FROM instructor
+LEFT OUTER JOIN student ON instructor.instructor_id = student.advisor_id
+WHERE student.advisor_id IS NULL;
