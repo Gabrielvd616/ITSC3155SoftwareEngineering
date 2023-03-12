c9216d53e5d23b6938c842ad2f73d12647141663
Gabriel Van Dreel <gvandree@uncc.edu>
2023-03-12 14:55:39 -0400

    Completed fifth exercise

---
+++ module9/exercise_05.sql
@@ -0,0 +1,4 @@
+SELECT instructor.first_name, instructor.last_name, SUM(course.num_credits)
+FROM instructor, course
+WHERE instructor.instructor_id = course.instructor_id
+GROUP BY course.instructor_id;
